b315622e80bae580ee03fb97de7b6e037dc20a6c
Juan <juancsilva86@gmail.com>
2026-04-15 12:40:50 -0400

    tudiando

---
+++ Backend/3.4 Middleware/index4.js
@@ -0,0 +1,24 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.post('/submit', (req, res) => {
+  res.send(`<h1>Your band name is:</h1></br><h2>${req.body.street + req.body.pet}✌</h2>`)
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
