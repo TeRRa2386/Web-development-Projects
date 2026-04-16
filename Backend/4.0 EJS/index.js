1bcf489c5c6b61c49dedc0afcad0f28d60f614a1
Juan <juancsilva86@gmail.com>
2026-04-16 13:08:06 -0400

    tudiando

---
+++ Backend/4.0 EJS/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+
+const app = express()
+const port = 3000
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.get('/', (req, res) => {
+    res.render(__dirname + '/views/index.ejs')
+})
+
+app.listen(port, () => {
+    console.log(`Listening port ${port}`)
+})
