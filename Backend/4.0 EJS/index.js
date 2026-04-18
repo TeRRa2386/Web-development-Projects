c06f1616a6b9a1ab7389ebaf257a6fae46f87176
TeRRa2386 <juancsilva86@gmail.com>
2026-04-18 00:12:42 -0400

    tudiando

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -1,16 +1,27 @@
-import express from 'express'
-import {dirname} from 'path'
-import {fileURLToPath} from 'url'
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+var answer = { first: "a weekday", second: "work hard" };
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+function dayChecker(req, res, next) {
+  var today = new Date();
+  var index = today.getDay();
+  console.log(index);
+  if (index === 0 || index === 6) {
+    answer.first = "the weekend";
+    answer.second = "have fun";
+  }
 
-app.get('/', (req, res) => {
-    res.render(__dirname + '/views/index.ejs')
-})
+  next();
+}
+
+app.use(dayChecker);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { answer: answer });
+});
 
 app.listen(port, () => {
-    console.log(`Listening port ${port}`)
-})
+  console.log(`Listening port ${port}`);
+});
