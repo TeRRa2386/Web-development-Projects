534653b5efd2cb0e8f272472b486b0072938afe5
Juan <juancsilva86@gmail.com>
2026-04-20 12:59:13 -0400

    tudiando

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -2,18 +2,20 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-var answer = { first: "a weekday", second: "work hard" };
+
+let answer = { first: "a weekday", second: "work hard" };
 
 function dayChecker(req, res, next) {
-  var today = new Date();
-  var index = today.getDay();
-  console.log(index);
-  if (index === 0 || index === 6) {
-    answer.first = "the weekend";
-    answer.second = "have fun";
-  }
+    
+    var today = new Date();
+    var index = today.getDay();
+    console.log(index);
+    if (index === 0 || index === 6) {
+        answer.first = "the weekend";
+        answer.second = "have fun";
+    }
 
-  next();
+    next();
 }
 
 app.use(dayChecker);
